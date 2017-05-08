f559a5c6c17a4fea37c4e1f4e76dd0d73dc5d1d2
MatiasComercio <mcomerciovazquez@gmail.com>
2017-05-08 18:33:23 -0300

    Add unit functions per layer (#8)
    - Migrate function usage for network creation
      - Add `create_all_non_linear_layers` method
        - This allows to simply assign the same `tanh` unit functions
          package to all the layers
      - Add `create_last_linear_layers` method
        - Same as the above, but last layer have linear unit functions package
    - Refactor code to match new specification

--- src/neural_network.m
+++ src/neural_network.m
@@ -14,28 +14,33 @@
 % - Structures cannot be modified inside functions. That's why we need to
 %   reassign them.
 
-function net = neural_network(neurons_per_layer, unit_functions)
+function net = neural_network(neurons_per_layer, are_close_enough, ...
+    cost_function)
 % neurons_per_layer: array that, for each position indicating a layer, 
-%   have how many neurons should that layer have. The input and 
-%   output layers must be included too (so, if you want to create a 
-%   neural network of M layers, neurons_per_layer should have length M+1)
-%   For example:
+%     specifies how many neurons should that layer have and the unit
+%     functions to be used on that layer. The input and
+%     output layers must be included too (so, if you want to create a
+%     neural network of M layers, neurons_per_layer should have length M+1).
+%   For example (only representing the number of neurons):
 %   	[ 2, 1 ] represents a single layer perceptron with 
 %       2 neurons on the input layer and one output neuron
 %     [ 4, 2, 3 ] represents a multilayer perceptron with 2 neurons on the
 %       input layer, one hidden layer with 2 neurons 
 %       and 3 neurons on the output layer
-% unit_functions: structure with the following definition
-%   unit_functions.g: activation function
-%   unit_functions.g_derivative: g derivative function
-%   unit_functions.cost_function: cost function
-%   unit_functions.are_close_enough: network output evaluation function
+%   Both parameters (number of neurons and unit functions for each layer)
+%     should be specified using a structure with the following fields:
+%   - neurons: amount of neurons for the current layer
+%   - g: activation function
+%   - g_derivative: g derivative function
+% are_close_enough: network output evaluation function
+% cost_function: used to calculate the error. Example: @mean_square_error
 
   net = struct;
   net.layers = create_layers(neurons_per_layer);
-  net.unit_functions = unit_functions;
+  net.cost_function = cost_function;
+  net.are_close_enough = are_close_enough;
   net.train = @train;
-  net.solve = @(net, pattern) solve(net.layers, net.unit_functions, pattern);
+  net.solve = @(net, pattern) solve(net.layers, pattern);
   % This is made public for testing purposes only
   net.fix = @fix;
 end
@@ -65,18 +70,20 @@ function layers = create_layers(neurons_per_layer)
   for m = 1:n_layers
     % Input size for current layer = neurons of current layer + 1 for 
     %   constant for bias neuron
-    input_length = neurons_per_layer(m) + 1;
+    input_length = neurons_per_layer(m).neurons + 1;
     % Amount of neurons on the current layer for processing the input
     % Note that the neurons of the current layer are the ones specified
     %   that the user specified for the next layer, as we noted before
-    n_neurons = neurons_per_layer(m + 1);
+    user_layer = neurons_per_layer(m+1);
+    n_neurons = user_layer.neurons;
     layers(m).weights = create_weights_matrix(n_neurons, input_length);
+    layers(m).g = user_layer.g;
+    layers(m).g_derivative = user_layer.g_derivative;
   end
 end
 
 function [net, train_memory] = train(net, patterns, expected_outputs, eta)
   layers = net.layers;
-  unit_functions = net.unit_functions;
 
   % Train until expected outputs match the neural outputs
   finished = false;
@@ -84,12 +91,12 @@ function [net, train_memory] = train(net, patterns, expected_outputs, eta)
   while ~finished
     % Train one complete epoch
     [layers, neural_outputs, epoch_memory] = ...
-        epoch(layers, patterns, expected_outputs, eta, unit_functions);
+        epoch(layers, patterns, expected_outputs, eta);
     % Calculate global error
-    global_error = unit_functions.cost_function(expected_outputs, neural_outputs);
+    global_error = net.cost_function(expected_outputs, neural_outputs);
     % Determine whether the outputs match
     finished = have_to_finish(expected_outputs, neural_outputs, ...
-        unit_functions);
+        net.are_close_enough);
     % Save current epoch parameters
     train_memory(epoch_i).layers = layers;
     train_memory(epoch_i).neural_outputs = neural_outputs;
@@ -100,9 +107,8 @@ function [net, train_memory] = train(net, patterns, expected_outputs, eta)
   net.layers = layers;
 end
 
-function [output, memory] = solve(layers, unit_functions, pattern)
+function [output, memory] = solve(layers, pattern)
 % layers: layers of a neural network
-% unit_functions: associated with the given neural network
 % pattern: column vector representing the input of the neural network
 %   It should have the declared length (same as when creating the neural
 %   network)
@@ -119,9 +125,8 @@ function [output, memory] = solve(layers, unit_functions, pattern)
      layer = layers(m);
      % Add the current bias factor as input too
      input = [ -1 ; memory(m).V ];
-     g = unit_functions.g; % TODO: move unit_functions to each layer
      % Output of current layer is the input of the next one
-     memory(m+1).V = g(layer.weights * input);
+     memory(m+1).V = layer.g(layer.weights * input);
   end
   output = memory(M + 1).V;
 end
@@ -130,11 +135,12 @@ end
 %% Below functions should act as `private` ones
 
 function [layers, memory] = fix(layers, expected_output, ...
-    output, g_derivative, solve_memory, eta)
+    output, solve_memory, eta)
   M = length(layers);
   g_output = output;
   % Calculate the delta for the last layer as a different case
-  memory(M).deltas = g_derivative(g_output) .* (expected_output - output);
+  memory(M).deltas = layers(M).g_derivative(g_output) .* ...
+      (expected_output - output);
   % Calculate deltas for all previous layers
   for m = M-1:-1:1
     % Output of the current layer is the input of the following
@@ -142,7 +148,8 @@ function [layers, memory] = fix(layers, expected_output, ...
     % Remove first column (i.e.: connections with the bias factor neuron)
     weights = layers(m+1).weights(:,2:end);
     deltas = memory(m+1).deltas;
-    memory(m).deltas = g_derivative(g_output) .* (weights.' * deltas);
+    memory(m).deltas = layers(m).g_derivative(g_output) .* ...
+        (weights.' * deltas);
   end
   % Fix all layers's weights
   for m = 1:M
@@ -162,7 +169,7 @@ function weights = create_weights_matrix(n_neurons, input_length)
 end
 
 function [layers, neural_outputs, memory] = epoch(layers, patterns, ...
-    expected_outputs, eta, unit_functions)
+    expected_outputs, eta)
   patterns_amount = columns(patterns);
   % Create a permutation to get a different sample patterns
   % Recall each pattern is a column vector, so, the number of columns of
@@ -177,10 +184,10 @@ function [layers, neural_outputs, memory] = epoch(layers, patterns, ...
     pattern = patterns(:,i);
     expected_output = expected_outputs(:,i);
     % Get the current pattern's output with the current weights
-    [output, solve_memory] = solve(layers, unit_functions, pattern);
+    [output, solve_memory] = solve(layers, pattern);
     % Fix weights for the current pattern
     [layers, fix_memory] = fix(layers, expected_output, output, ...
-      unit_functions.g_derivative, solve_memory, eta);
+      solve_memory, eta);
     % Save current pattern training results
     memory = save_to_epoch_memory(memory, i, output, solve_memory, ...
         fix_memory);
@@ -195,13 +202,11 @@ function mem = save_to_epoch_memory(mem, i, output, solve_mem, fix_mem)
 end
 
 function outputs_match = have_to_finish(expected_outputs, ...
-    neural_outputs, unit_functions)
-  are_close_enough = unit_functions.are_close_enough;
-  epsilon = unit_functions.epsilon;
+    neural_outputs, are_close_enough)
   outputs_match = true;
   for i = 1:columns(expected_outputs)
     outputs_match = outputs_match && ...
-        are_close_enough(expected_outputs(i), neural_outputs(i), epsilon);
+        are_close_enough(expected_outputs(i), neural_outputs(i));
     if ~ outputs_match
       return;
     end
--- src/perceptrons/parity_perceptron.m
+++ src/perceptrons/parity_perceptron.m
@@ -51,20 +51,17 @@ expected_outputs = [
 ];
 
 eta = 0.05;
+epsilon = .1;
 
 % Define which unit functions are going to be used
-unit_function = 'non_linear_tanh';
-g = str2func(strcat(unit_function, '_g'));
-g_derivative = str2func(strcat(unit_function, '_g_derivative'));
-are_close_enough = str2func(strcat(unit_function, '_are_close_enough'));
+are_close_enough = str2func(strcat(unit_function, '_are_close_enough')); % TODO: migrate this outside the activation_functions folder
+with_epsilon_are_close_enough = @(expected_output, neural_output) ...
+    (are_close_enough(expected_output, neural_output, epsilon));
 cost_function = @mean_square_error;
-unit_functions.g = g;
-unit_functions.g_derivative = g_derivative;
-unit_functions.are_close_enough = are_close_enough;
-unit_functions.cost_function = cost_function;
-unit_functions.epsilon = .1;
 
-net = neural_network([rows(patterns), 7, rows(expected_outputs)], unit_functions);
+layers = create_all_non_linear_layers...
+    ([rows(patterns), 7, rows(expected_outputs)]);
+net = neural_network(layers, with_epsilon_are_close_enough, cost_function);
 
 % Train the network
 [net, train_memory] = net.train(net, patterns, expected_outputs, eta);
